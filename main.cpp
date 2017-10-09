91da39bf993093287f3c615fe054c5a0883358dc
NoblFriend <32647360+NoblFriend@users.noreply.github.com>
2017-10-09 21:30:59 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,153 @@
-#include <gl/gl.h>
-#include <windows.h>
-#include "myownglut.cpp"      //    ^^
-#include "myownwindowslib.cpp"//    :3
-#include "object.cpp"
+#include <Windows.h>
+#include "glut.h"
+#include <vector>
+
+struct basematerial {
+    int r=0,g=0,b=0,a=0;
+    char HEX_color[8];
+    basematerial(char color[8]) {
+        for (int i = 0; i<8; i++) {
+            HEX_color[i] = color[i];
+            switch (i) {
+                case 1: r+=color[i];break;
+                case 2: r+=color[i];break;
+                case 3: g+=color[i];break;
+                case 4: g+=color[i];break;
+                case 5: b+=color[i];break;
+                case 6: b+=color[i];break;
+                case 7: a+=color[i];break;
+                case 8: a+=color[i];break;
+            }
+        }
+    }
+    basematerial() {}
+};
+
+struct materials {
+    std::vector<basematerial> basematerials;
+    /* std::vector<texture> textures; */
+    materials( std::vector<basematerial> _basematerials ) {
+        basematerials = _basematerials;
+    }
+    materials() {}
+};
+
+struct vertex {
+    double x,y,z;
+    vertex(double _x, double _y, double _z)
+    {
+        x = _x;
+        y = _y;
+        z = _z;
+    }
+    vertex() {}
+};
+
+struct triangle {
+    int vertices[3] {0,0,0};
+    int material = 0;
+    triangle(int first, int second, int third, int _material) {
+        vertices[0] = first;
+        vertices[1] = second;
+        vertices[2] = third;
+        material = _material;
+    }
+    triangle(int first, int second, int third) {
+        vertices[0] = first;
+        vertices[1] = second;
+        vertices[2] = third;
+    }
+};
+
+struct object {
+	std::vector<vertex> vertices = {};
+	std::vector<triangle> triangles = {};
+    object(std::vector<vertex> _vertices, std::vector<triangle> _triangles) {
+        for (int i = 0; i < _vertices.size(); i++) {
+            vertices.push_back (_vertices[i]); 
+        }
+        for (int i = 0; i < _triangles.size(); i++) {
+            triangles.push_back(_triangles[i]);
+        }
+    }
+    void draw () {
+        glPushMatrix();
+            glBegin(GL_TRIANGLES);
+                for (int i = 0; i < triangles.size(); i++) {
+                    for (int v = 0; v < 3; v++)
+                        glVertex3d(vertices[triangles[i].vertices[v]].x,vertices[triangles[i].vertices[v]].y,vertices[triangles[i].vertices[v]].z);
+                }
+            glEnd();
+        glPopMatrix();
+    }
+};
+
+void EnableOpenGL(HWND hwnd, HDC* hDC, HGLRC* hRC) {
+    PIXELFORMATDESCRIPTOR pfd = { sizeof(PIXELFORMATDESCRIPTOR) };
+
+    int iFormat;
+
+    *hDC = GetDC(hwnd);
+
+
+    pfd.nSize = sizeof(pfd);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    pfd.iPixelType = PFD_TYPE_RGBA;
+    pfd.cColorBits = 24;
+    pfd.cDepthBits = 16;
+    pfd.iLayerType = PFD_MAIN_PLANE;
+
+    iFormat = ChoosePixelFormat(*hDC, &pfd);
+
+    SetPixelFormat(*hDC, iFormat, &pfd);
+
+    *hRC = wglCreateContext(*hDC);
+
+    wglMakeCurrent(*hDC, *hRC);
+}
+
+void DisableOpenGL (HWND hwnd, HDC hDC, HGLRC hRC){
+    wglMakeCurrent(NULL, NULL);
+    wglDeleteContext(hRC);
+    ReleaseDC(hwnd, hDC);
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT Msg, WPARAM wParam, LPARAM lParam) {
+    switch (Msg) {
+        case WM_CLOSE: PostQuitMessage(0);break;
+
+        case WM_DESTROY:
+            return 0;
+
+        case WM_KEYDOWN:
+            switch (wParam) {
+                case VK_ESCAPE:PostQuitMessage(0);break;
+            }break;
+
+        default:
+            return DefWindowProc(hwnd, Msg, wParam, lParam);
+    }
+
+    return 0;
+}
+
+WNDCLASSEX GLSample () {
+    WNDCLASSEX wc = { sizeof(WNDCLASSEX) };
+
+    wc.style = CS_OWNDC;
+    wc.lpfnWndProc = WindowProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = CreateSolidBrush(RGB(0,128,128));
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = "GLSample";
+    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);;
+
+    return wc;
+}
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     WNDCLASSEX wc = GLSample();
@@ -12,11 +157,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmd
     MSG msg;
     BOOL bQuit = FALSE;
 
-    vertex vertices[4] = {vertex(0,0,0),vertex(1,0,0),vertex(0,1,0),vertex(0,0,1)};
-    triangle triangles[4] = {triangle(0,1,2),triangle(0,1,3),triangle(0,2,3),triangle(1,2,3)};
+    std::vector<vertex> vertices = {vertex(0,0,0),vertex(1,0,0),vertex(0,1,0),vertex(0,0,1)};
+    std::vector<triangle> triangles = {triangle(0,1,2),triangle(0,1,3),triangle(0,2,3),triangle(1,2,3)};
     object obj = object(vertices,triangles);
 
-    double phi;
+    double phi = 0;
 
     if (!RegisterClassEx(&wc))
         return 1;
